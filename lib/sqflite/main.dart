ed91b5f7141773b10c862e13dafe15c1e0ae5da8
Subi-Babu <subiaby4613@gmail.com>
2022-12-29 19:16:45 +0530

    up

--- lib/sqflite/main.dart
+++ lib/sqflite/main.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'sql_helper.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
